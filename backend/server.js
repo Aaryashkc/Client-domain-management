c881d007f5280b9d4f58469fd81dc45be35ea303
Aaryashkc <aaryashkc123@gmail.com>
2025-03-25 12:55:32 +0545

    third

--- backend/server.js
+++ backend/server.js
@@ -31,10 +31,9 @@ app.use('/api/service-provider', providerRoutes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/dist')));
-
-    app.get('*', (res, req =>{
+    app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
-    }))
+    });
 }
 
 
